458edf6e93360599ddee42a9ebfdfb93a12c3307
Amritpal singh <amr.kamboj@yahoo.in>
2014-08-05 16:16:49 +0530

    Update saved.js

--- js/saved.js
+++ js/saved.js
@@ -31,8 +31,8 @@ function SavedViewModel(items)
 }
 
 $(document).ready(function () {
-    document.addEventListener("deviceready", onDeviceReady, false);
-    //onDeviceReady();
+    //document.addEventListener("deviceready", onDeviceReady, false);
+    onDeviceReady();
 });
 
 function onDeviceReady()
